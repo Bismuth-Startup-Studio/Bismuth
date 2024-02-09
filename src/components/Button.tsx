f03e73c12daee3ad8a2c3c53cdda232947bb98da
FiifiYawson <ghyawson@gmail.com>
2024-02-09 15:28:46 +0000

    removed faulty component

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({children , red, type, styles}) {
-  return (
-    <button type={type} className={`block ${red?"text-white":"text-secondary-red"} ${red?"bg-primary-red":"bg-white"} py-3 px-[20px] rounded-full m-auto mt-[30px] text-[24px] font-[700] ${styles}`}>{children}</button>
-  )
-}
-
-export default Button
